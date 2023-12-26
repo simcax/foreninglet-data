06d872011bb39fdf7bbc3f28f1ad667acf840fa9
Carsten Skov <carsten@simcax.dk>
2023-12-26 21:22:20 +0100

    Added counts for members and genders to the memberlist class

--- foreninglet_data/memberlist.py
+++ foreninglet_data/memberlist.py
@@ -1,6 +1,9 @@
 """
 Class for handling the ForeningLet Memberlist data
 """
+import json
+
+import pandas as pd
 
 
 class Memberlist:
@@ -10,5 +13,25 @@ class Memberlist:
     as it is returned from the API.
     """
 
-    def __init__(self) -> None:
-        pass
+    memberlist = ""
+    member_count = 0
+    count_men = 0
+    count_women = 0
+
+    def __init__(self, memberlist) -> None:
+        self.memberlist = memberlist
+        self.member_count = len(memberlist)
+        self._count_genders()
+
+    def _count_genders(self):
+        """
+        Method to count the number of men and women in the memberlist
+        Men are signified by 'Gender' = 'M' (danish - M for Mand)
+        Women are signified by 'Gender' = 'K' (danish - K for Kvinde)
+        """
+        the_memberlist = self.memberlist
+        if isinstance(the_memberlist, list):
+            the_memberlist = json.dumps(self.memberlist)
+        df = pd.read_json(the_memberlist)
+        self.count_men = df["Gender"].value_counts()["M"]
+        self.count_women = df["Gender"].value_counts()["K"]
--- tests/test_memberlist.py
+++ tests/test_memberlist.py
@@ -1,12 +1,48 @@
 """
 Testing the ForeningLet Data Memberlist class
 """
-from foreninglet_data import memberlist
+import json
+
+import pandas as pd
+
+from foreninglet_data.memberlist import Memberlist
 
 
-def test_memberlist_membercount():
+def test_memberlist_isobject():
     """
     A test to see a memberlist object has the correct membercount attribute
     """
-    memberlist_obj = memberlist.Memberlist()
+    memberlist_obj = Memberlist()
     assert isinstance(memberlist_obj, object)
+
+
+def test_memberlist_loads_memberlist(mocked_memberlist):
+    """
+    Test to make sure the passed memberlist is held by the memberlist object
+    """
+    memberlist = mocked_memberlist(5)
+    memberlist_obj = Memberlist(memberlist)
+    assert memberlist_obj.memberlist == memberlist
+
+
+def test_memberlist_has_correct_membercount(mocked_memberlist):
+    """
+    Tests the memberlist object to initialize the count of members as an attribute
+    """
+    memberlist = mocked_memberlist(11)
+    memberlist_obj = Memberlist(memberlist)
+    assert memberlist_obj.member_count == 11
+
+
+def test_memberlist_has_correct_gender_count(mocked_memberlist):
+    """
+    Tests the memberlist object to initialize the count of members for each gender
+    as attributes
+    """
+    memberlist = mocked_memberlist(11)
+    df = pd.read_json(json.dumps(memberlist))
+    males = df["Gender"].value_counts()["M"]
+    females = df["Gender"].value_counts()["K"]
+    memberlist_obj = Memberlist(memberlist)
+    assert memberlist_obj.count_men == males
+    assert memberlist_obj.count_women == females
